c05dbc7a00be8a9972ef09a38226b32d9270be46
Jono Wilkinson <jw@jettylabs.com>
2018-10-25 12:48:55 +1100

    Adds basic start script with intro message;  Adds package.json;

---
+++ app/start.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let intro  = ('\n\n');
+    intro += ('---------------------------------------\n');
+    intro += ('     T O T E   C A L C U L A T O R\n');
+    intro += ('---------------------------------------\n');
+    intro += ('\n\n');
+    intro += ('Welcome. Please provide bets or results.\n');
+    intro += ('\n\n');
+
+process.stdout.write(intro);
