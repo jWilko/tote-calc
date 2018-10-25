96c7a08a2596f1af7f4f423e969f8aa4048e4d7b
Jono Wilkinson <jw@jettylabs.com>
2018-10-25 15:16:32 +1100

    Adds framework for resulter initialisation

---
+++ app/utils/resulter.util.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const logger = require('./logger.util.js');
+
+const util = {};
+
+let inProgress;
+
+util.isInProgress = () => {
+    return !!inProgress;
+};
+
+util.init = (resultLine) => {
+    inProgress = true;
+    logger(`Result instruction received : ${resultLine} \n`);
+    // TODO : Add resulting logic
+    process.exit(0);
+};
+
+module.exports = util;
---
+++ app/utils/resulter.util.spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+
+describe('Resulter utility', () => {
+    let util;
+
+    beforeEach(() => {
+        const dependencyStubs = {
+            './logger.util.js' : sinon.stub()
+        };
+        util = proxyquire('./resulter.util.js', dependencyStubs);
+    });
+
+    describe('isInProgress()', () => {
+        it('returns false when processing is not in progress', () => {
+            expect(util.isInProgress()).to.equal(false);
+        });
+        it('returns true when processing is in progress', () => {
+            util.init();
+            expect(util.isInProgress()).to.equal(true);
+        });
+    });
+
+    describe('init()', () => {
+        it('sets inProgress flag to true', () => {
+            util.init();
+            expect(util.isInProgress()).to.equal(true);
+        });
+
+    });
+
+});
