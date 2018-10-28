7b6b485107b567fbea338b910638f917b5a5b475
Jono Wilkinson <jw@jettylabs.com>
2018-10-28 20:44:08 +1100

    Updates input handler to parse provided data against config regex and other rules to determine how to handle the provided input.

--- app/utils/inputHandler.util.js
+++ app/utils/inputHandler.util.js
@@ -1,10 +1,42 @@
 'use strict';
 
-const util = (data) => {
-    const dataStr = data.toString().trim();
+const config = require('../config.json');
+const betManager = require('./betManager.util.js');
+const resulter = require('./resulter.util.js');
+const logger = require('./logger.util.js');
 
-    process.stdout.write(`Received some data : ${dataStr} \n`);
-    process.exit();
+const util = (data) => {
+    data = data.trim();
+
+    // Ignore empty lines
+    if(!data) {
+        return;
+    }
+
+    // Don't allow action if Results are being processed
+    if(resulter.isInProgress()) {
+        logger(`UNAVAILABLE : The application is currently processing results. Unable to process ${data}\n`);
+        return;
+    }
+
+    const betRegex = new RegExp(config.regex.bet);
+    const resultRegex = new RegExp(config.regex.result);
+
+    if(betRegex.test(data)) {
+        betManager.addBet(data);
+
+    } else if(resultRegex.test(data)) {
+        resulter.init(data);
+
+    } else if(data === 'exit') {
+        logger(`\n\nShutting down. Total bets : ${betManager.getAllBets().length} \n\n`);
+        return process.exit(0);
+
+    } else {
+        logger('ERROR : Illegal command received. Shutting down.\n');
+        return process.exit(0);
+
+    }
 };
 
 module.exports = util;
--- app/utils/inputHandler.util.spec.js
+++ app/utils/inputHandler.util.spec.js
@@ -1,16 +1,156 @@
 'use strict';
 
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
 
 describe('Input Handler utility', () => {
     let util;
+    let stubs;
+    let inputData;
 
     beforeEach(() => {
-        util = require('./inputHandler.util.js');
+        stubs = {
+            betManager : {
+                addBet : sinon.stub(),
+                getAllBets : sinon.stub().returns([])
+            },
+            resulter : {
+                isInProgress : sinon.stub().returns(false),
+                init : sinon.stub()
+            },
+            logger : sinon.stub(),
+            processExit : sinon.stub(process, 'exit')
+        };
+        // Stub the dependencies
+        util = proxyquire('./inputHandler.util.js', {
+            './betManager.util.js' : stubs.betManager,
+            './resulter.util.js' : stubs.resulter,
+            './logger.util.js' : stubs.logger
+        });
+    });
+
+    afterEach(() => {
+        process.exit.restore();
     });
 
     it('exists as a function', () => {
         expect(typeof util).to.equal('function');
+    });
+
+    describe('when resulting is in progress', () => {
+        beforeEach(() => {
+            stubs.resulter.isInProgress.returns(true);
+            inputData = 'some data';
+            util(inputData);
+        });
+        it('does not create a bet', () => {
+            expect(stubs.betManager.addBet.callCount).to.equal(0);
+        });
+        it('does not initialise another resulter', () => {
+            expect(stubs.resulter.init.callCount).to.equal(0);
+        });
+        it('does log a message', () => {
+            expect(stubs.logger.callCount).to.equal(1);
+        });
+        it('does not kill the process', () => {
+            expect(stubs.processExit.callCount).to.equal(0);
+        });
+    });
+
+    describe('when resulting is not in progress', () => {
+        describe('when the provided data is an empty string', () => {
+            beforeEach(() => {
+                inputData = '  ';
+                util(inputData);
+            });
+            it('does not create a bet', () => {
+                expect(stubs.betManager.addBet.callCount).to.equal(0);
+            });
+            it('does not initialise a resulter', () => {
+                expect(stubs.resulter.init.callCount).to.equal(0);
+            });
+            it('does not log a message', () => {
+                expect(stubs.logger.callCount).to.equal(0);
+            });
+            it('does not kill the process', () => {
+                expect(stubs.processExit.callCount).to.equal(0);
+            });
+        });
+        describe('when the data matches the Bet regex', () => {
+            beforeEach(() => {
+                inputData = 'Bet:w:2:35';
+                util(inputData);
+            });
+            it('does create a bet', () => {
+                expect(stubs.betManager.addBet.callCount).to.equal(1);
+                expect(stubs.betManager.addBet.args[0][0]).to.equal(inputData);
+            });
+            it('does not initialise a resulter', () => {
+                expect(stubs.resulter.init.callCount).to.equal(0);
+            });
+            it('does not log a message', () => {
+                expect(stubs.logger.callCount).to.equal(0);
+            });
+            it('does not kill the process', () => {
+                expect(stubs.processExit.callCount).to.equal(0);
+            });
+        });
+        describe('when the data matches the Result regex', () => {
+            beforeEach(() => {
+                inputData = 'Result:9:4:16';
+                util(inputData);
+            });
+            it('does not create a bet', () => {
+                expect(stubs.betManager.addBet.callCount).to.equal(0);
+            });
+            it('does initialise a resulter', () => {
+                expect(stubs.resulter.init.callCount).to.equal(1);
+                expect(stubs.resulter.init.args[0][0]).to.equal(inputData);
+            });
+            it('does not log a message', () => {
+                expect(stubs.logger.callCount).to.equal(0);
+            });
+            it('does not kill the process', () => {
+                expect(stubs.processExit.callCount).to.equal(0);
+            });
+        });
+        describe('when the provided data indicates exit', () => {
+            beforeEach(() => {
+                inputData = 'exit';
+                util(inputData);
+            });
+            it('does not create a bet', () => {
+                expect(stubs.betManager.addBet.callCount).to.equal(0);
+            });
+            it('does not initialise a resulter', () => {
+                expect(stubs.resulter.init.callCount).to.equal(0);
+            });
+            it('does log a message', () => {
+                expect(stubs.logger.callCount).to.equal(1);
+            });
+            it('does kill the process', () => {
+                expect(stubs.processExit.callCount).to.equal(1);
+            });
+        });
+        describe('when the provided data is not a recognised string', () => {
+            beforeEach(() => {
+                inputData = 'some data';
+                util(inputData);
+            });
+            it('does not create a bet', () => {
+                expect(stubs.betManager.addBet.callCount).to.equal(0);
+            });
+            it('does not initialise a resulter', () => {
+                expect(stubs.resulter.init.callCount).to.equal(0);
+            });
+            it('does log a message', () => {
+                expect(stubs.logger.callCount).to.equal(1);
+            });
+            it('does kill the process', () => {
+                expect(stubs.processExit.callCount).to.equal(1);
+            });
+        });
     });
 
 });
