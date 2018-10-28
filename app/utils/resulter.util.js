bbcced119dfec8791ef2d524e1ba7245a9d0d204
Jono Wilkinson <jw@jettylabs.com>
2018-10-29 00:11:38 +1100

    Adds resulting logic.

--- app/utils/resulter.util.js
+++ app/utils/resulter.util.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const betManager = require('./betManager.util.js');
+const poolManager = require('./poolManager.util.js');
 const logger = require('./logger.util.js');
 
 const util = {};
@@ -13,9 +15,81 @@ util.isInProgress = () => {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 util.init = (resultLine) => {
+    logger(`\n\nResult instruction received : ${resultLine} \n`);
     inProgress = true;
-    logger(`Result instruction received : ${resultLine} \n`);
-    // TODO : Add resulting logic
+
+    const resultParts = resultLine.split(':');
+    const result = {
+        first : resultParts[1],
+        second : resultParts[2],
+        third : resultParts[3],
+        exacta : `${resultParts[1]},${resultParts[2]}`
+    };
+
+    const payablePools = poolManager.getFinalPayablePools();
+    const allBets = betManager.getAllBets();
+
+    // find the winning bets > aggregate stakes per product/position.
+    const winningStake = {
+        W : 0,
+        P1 : 0,
+        P2 : 0,
+        P3 : 0,
+        E : 0
+    };
+    allBets.forEach((bet) => {
+        if(bet.product === 'W' && bet.selection === result.first) {
+            winningStake.W += bet.stake;
+        }
+        if(bet.product === 'P') {
+            if(bet.selection === result.first) {
+                winningStake.P1 +=bet.stake;
+            }
+            if(bet.selection === result.second) {
+                winningStake.P2 += bet.stake;
+            }
+            if(bet.selection === result.third) {
+                winningStake.P3 += bet.stake;
+            }
+        }
+        if(bet.product === 'E' && bet.selection === result.exacta) {
+            winningStake.E += bet.stake;
+        }
+    });
+
+    const dividend = {
+        W : 0,
+        P1 : 0,
+        P2 : 0,
+        P3 : 0,
+        E : 0,
+    };
+
+    // Calculate dividends by dividing total stake into payable pool; 'Place' pool divided evenly amongst P1-P2-P3
+    if(winningStake.W) {
+        dividend.W = (payablePools.W / winningStake.W);
+    }
+    // TODO : Likely bug in P1-P2-P3 calcs - if there are no winning bets in one or more, probably shouldn't divive by 3
+    if(winningStake.P1) {
+        dividend.P1 = (payablePools.P / 3 / winningStake.P1);
+    }
+    if(winningStake.P2) {
+        dividend.P2 = (payablePools.P / 3 / winningStake.P2);
+    }
+    if(winningStake.P3) {
+        dividend.P3 = (payablePools.P / 3 / winningStake.P3);
+    }
+    if(winningStake.E) {
+        dividend.E = (payablePools.E / winningStake.E);
+    }
+
+    logger('Results:\n\n');
+    logger(`Win:${result.first}:${dividend.W.toFixed(2)}\n`);
+    logger(`Place:${result.first}:${dividend.P1.toFixed(2)}\n`);
+    logger(`Place:${result.second}:${dividend.P2.toFixed(2)}\n`);
+    logger(`Place:${result.third}:${dividend.P3.toFixed(2)}\n`);
+    logger(`Exacta:${result.exacta}:${dividend.E.toFixed(2)}\n\n`);
+
     process.exit(0);
 };
 
--- app/utils/resulter.util.spec.js
+++ app/utils/resulter.util.spec.js
@@ -6,12 +6,23 @@ const proxyquire = require('proxyquire');
 
 describe('Resulter utility', () => {
     let util;
+    let stubs;
 
     beforeEach(() => {
-        const dependencyStubs = {
-            './logger.util.js' : sinon.stub()
+        stubs = {
+            betManager : {
+                getAllBets : sinon.stub().returns([])
+            },
+            poolManager : {
+                getFinalPayablePools : sinon.stub().returns({})
+            },
+            logger : sinon.stub()
         };
-        util = proxyquire('./resulter.util.js', dependencyStubs);
+        util = proxyquire('./resulter.util.js', {
+            './betManager.util.js' : stubs.betManager,
+            './poolManager.util.js' : stubs.poolManager,
+            './logger.util.js' : stubs.logger
+        });
         sinon.stub(process, 'exit');
     });
 
@@ -25,16 +36,109 @@ describe('Resulter utility', () => {
             expect(util.isInProgress()).to.equal(false);
         });
         it('returns true when processing is in progress', () => {
-            util.init();
+            util.init('Result:1:2:3');
             expect(util.isInProgress()).to.equal(true);
         });
     });
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     describe('init()', () => {
+
         it('sets inProgress flag to true', () => {
-            util.init();
+            util.init('Result:1:2:3');
             expect(util.isInProgress()).to.equal(true);
+        });
+
+        it('gets the final payable pool amount from the poolManager', () => {
+            util.init('Result:1:2:3');
+            expect(stubs.poolManager.getFinalPayablePools.callCount).to.equal(1);
+        });
+
+        it('gets all bets from the betManager', () => {
+            util.init('Result:1:2:3');
+            expect(stubs.betManager.getAllBets.callCount).to.equal(1);
+        });
+
+        describe('calculates... ', () => {
+            beforeEach(() => {
+                stubs.poolManager.getFinalPayablePools.returns({
+                    W : 85,
+                    P : 264,
+                    E : 82,
+                });
+                stubs.betManager.getAllBets.returns([
+                    {
+                        product:'W',
+                        selection : '9',
+                        stake: 90
+                    },
+                    {
+                        product:'W',
+                        selection : '3',
+                        stake: 2
+                    },
+                    {
+                        product:'W',
+                        selection : '3',
+                        stake: 8
+                    },
+
+
+                    {
+                        product:'P',
+                        selection : '3',
+                        stake: 4
+                    },
+                    {
+                        product:'P',
+                        selection : '3',
+                        stake: 6
+                    },
+                    {
+                        product:'P',
+                        selection : '5',
+                        stake: 10
+                    },
+                    {
+                        product:'P',
+                        selection : '6',
+                        stake: 12
+                    },
+                    {
+                        product:'P',
+                        selection : '6',
+                        stake: 2
+                    },
+                    {
+                        product:'P',
+                        selection : '6',
+                        stake: 6
+                    },
+                    {
+                        product:'E',
+                        selection : '3,6',
+                        stake: 10
+                    }
+
+                ]);
+                util.init('Result:3:6:5');
+            });
+            it('calculates the WIN product result accurately', () => {
+                expect(stubs.logger.args[2][0]).to.contain('Win:3:8.50');
+            });
+            it('calculates the PLACE-First product result accurately', () => {
+                expect(stubs.logger.args[3][0]).to.contain('Place:3:8.80');
+            });
+            it('calculates the PLACE-Second product result accurately', () => {
+                expect(stubs.logger.args[4][0]).to.contain('Place:6:4.40');
+            });
+            it('calculates the PLACE-Third product result accurately', () => {
+                expect(stubs.logger.args[5][0]).to.contain('Place:5:8.80');
+            });
+            it('calculates the EXACTA product result accurately', () => {
+                expect(stubs.logger.args[6][0]).to.contain('Exacta:3,6:8.20');
+            });
+
         });
 
     });
